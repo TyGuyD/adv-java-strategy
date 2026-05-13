8be6bc573adc0cddc89033d51e2f8624a1b419f3
TylerD <tyguy.demshar@gmail.com>
2026-05-13 15:10:14 -0500

    composition

--- src/composition/Student.java
+++ src/composition/Student.java
@@ -1,12 +1,13 @@
 package composition;
 
-public class Student extends Address {
+public class Student {
 
+    private Address address;
     private String name;
     private int age;
 
     public Student(String streetAddress, int zipCode, String state, String phone, String name, int age) {
-        super(streetAddress, zipCode, state, phone);
+        this.address = new Address(streetAddress, zipCode, state, phone);
         this.name = name;
         this.age = age;
     }
@@ -26,4 +27,23 @@ public class Student extends Address {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getPhone() {
+        return address.getPhone();
+    }
+
+    public void setPhone(String phone) {address.setPhone(phone);}
+
+    public int getZipCode() {return address.getZipCode();}
+
+    public void setZipCode(int zipCode) {address.setZipCode(zipCode);}
+
+    public Address getAddress() {return address;}
+
+    public void setAddress(Address address) {this.address = address;}
+
+    public String getState() {return address.getState();}
+
+    public void setState(String state) {address.setState(state);}
+
 }
